e62216da2712b3c32304b3afe00a41fb63711207
balaji bharath <balajibharath511@gmail.com>
2024-04-04 11:25:35 +0530

    updating product.cs

--- onlineRetail/Model/Product.cs
+++ onlineRetail/Model/Product.cs
@@ -1,11 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace onlineRetail.Model
 {
     public class Product
     {
         [Key]
+     
         public Guid productId { get; set; }
+        [Required]
         public string ProductName { get; set; }
         public int quantity { get; set; }
         public bool isActive { get; set; }
